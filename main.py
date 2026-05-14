ab50adfc541c99680a20a558cc9e0a48179724c7
Student <student@compass.local>
2026-05-14 11:01:00 +0600

    product main

--- main.py
+++ main.py
@@ -5,18 +5,7 @@ import uvicorn
 from src.database import Base, engine
 from src.products.router import product_router
 from src.auth.router import user_router, auth
-from src.products.router import router as products_router
-app.include_router(products_router)
 
-# from src.models.user import User
-# from src.models.product import Product
-# from src.models.order import Order
-# from src.models.comment import Comment
-
-# User.metadata.create_all(bind=engine)
-# Product.metadata.create_all(bind=engine)
-# Order.metadata.create_all(bind=engine)
-# Comment.metadata.create_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
